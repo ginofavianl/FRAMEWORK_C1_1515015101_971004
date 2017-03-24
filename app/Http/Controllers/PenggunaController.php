5370fc7745a3ba14724abfe1cb866a9a8bc69fbd
ginofavianl <ginoleon97@gmail.com>
2017-03-24 23:48:48 +0800

    Postest 4 Selesai

---
+++ app/Http/Controllers/PenggunaController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\pengguna;
+
+class PenggunaController extends Controller
+{
+public function awal()
+
+	{
+    return view('pengguna.awal',['data'=>Pengguna::all()]);
+	}
+public function tambah()
+	{
+		return view('pengguna.tambah');
+	}
+public function simpan(Requests $input)
+	{
+		$pengguna = new Pengguna();
+		$pengguna->username = 'gino';
+		$pengguna->password = 'leony';
+		$pengguna->save();
+		return "Data Dengan Username {$pengguna->username} telah  disimpan";
+	}
+public function edit($id)
+	{
+		$pengguna = Pengguna::find($id);
+		return view('pengguna.edit')->with(array('pengguna'=>pengguna));
+	}
+public function lihat($id)
+	{
+		$pengguna = Pengguna::find($id);
+		return view('pengguna.lihat')->with(array('pengguna'=>pengguna));
+	}
+public function update($id, Request $input)
+	{
+		$pengguna = Pengguna::find($id);
+		$pengguna->username = $input->username;
+		$pengguna->password = $input->password;
+		$informasi = $pengguna->save() ? 'Berhasil Update Data' : 'Gagal Update Data';
+		return redirect('pengguna')->with(['informasi'=>$informasi]);
+	}
+public function hapus($id)
+	{
+		$pengguna = Pengguna::find($id);
+		$informasi = $pengguna->delete() ? 'Berhasil Hapus Data' : 'Gagal Hapus Data';
+		return redirect('pengguna')->with(['infomarsi'=>informasi]);
+	}
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,185 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('pengguna','PenggunaController@awal');
+Route::get('pengguna/tambah','PenggunaController@tambah');
+
+Route::get('dosen','DosenController@awal');
+Route::get('dosen/tambah','DosenController@tambah');
+
+Route::get('dosen_matakuliah','Dosen_MatakuliahController@awal');
+Route::get('dosen_matakuliah/tambah','Dosen_MatakuliahController@tambah');
+
+Route::get('jadwal_matakuliah','Jadwal_MatakuliahController@awal');
+Route::get('jadwal_matakuliah/tambah','Jadwal_MatakuliahController@tambah');
+
+Route::get('mahasiswa','MahasiswaController@awal');
+Route::get('mahasiswa/tambah','MahasiswaController@tambah');
+
+Route::get('matakuliah','MatakuliahController@awal');
+Route::get('matakuliah/tambah','MatakuliahController@tambah');
+
+Route::get('ruangan','RuanganController@awal');
+Route::get('ruangan/tambah','RuanganController@tambah');
+
+Route::get('pengguna/add',function(){
+		$pengguna = new App\pengguna();
+		$pengguna->username = 'yo';
+		$pengguna->password = 'hihi';
+		$pengguna->save();
+		return "Data Dengan Username {$pengguna->username} telah  disimpan";
+	});
+
+Route::get('/', function () {
+    return view ('master');
+});
+
+Route::get('/hello-world', function () {
+    return 'Doni Huhahahahahaha';
+});
+
+Route::get('/coba/{var}', function($variabel){
+	return $variabel;
+});
+
+
+Route::get('/hello/{var?}', function($variabel = "Belum ada isinya"){
+	return $variabel;
+});
+
+//------------------------------------------------------------------
+
+Route::get('/pengguna', 'PenggunaController@awal');
+
+Route::get('/pengguna/tambah', 'PenggunaController@tambah');
+
+//------------------------------------------------------------------
+
+Route::get('/dosen', 'DosenController@awal');
+
+Route::get('dosen/tambah', 'DosenController@tambah');
+
+//------------------------------------------------------------------
+
+Route::get('/mahasiswa', 'MahasiswaController@awal');
+
+Route::get('/mahasiswa/tambah', 'MahasiswaController@tambah');
+
+Route::get('/mahasiswa/tambah/route', function(){
+		$mahasiswa = new App\Mahasiswa();
+        $mahasiswa->nama = 'Indra Wijaya';
+        $mahasiswa->nim = '1515015121';
+        $mahasiswa->alamat = 'Jl. Lambung Mangkuratadaw';
+        $mahasiswa->pengguna_id = 3;
+        $mahasiswa->save();
+        return "Telah disave {$mahasiswa->nama} ke dalam databas";
+});
+
+//------------------------------------------------------------------
+
+Route::get('/ruangan', 'RuanganController@awal');
+
+Route::get('/ruangan/tambah', 'RuanganController@tambah');
+
+//------------------------------------------------------------------
+
+Route::get('/matakuliah', 'MatakuliahController@awal');
+
+Route::get('/matakuliah/tambah', 'MatakuliahController@tambah');
+
+//------------------------------------------------------------------
+
+Route::get('/dosen_matakuliah', 'Dosen_matakuliahController@awal');
+
+Route::get('/dosen_matakuliah/tambah', 'Dosen_MatakuliahController@tambah');
+
+//------------------------------------------------------------------
+
+Route::get('/jadwal_matakuliah', 'Jadwal_matakuliahController@awal');
+
+Route::get('/jadwal_matakuliah/tambah', 'Jadwal_matakuliahController@tambah');
+
+//------------------------------------------------------------------
+
+Route::get('pengguna/lihat/{pengguna}', 'PenggunaController@lihat');
+Route::post('pengguna/simpan', 'PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}', 'PenggunaController@edit');
+Route::post('pengguna/edit/{pengguna}', 'PenggunaController@update');
+Route::get('pengguna/hapus/{pengguna}', 'PenggunaController@hapus');
+
+//------------------------------------------------------------------
+
+Route::get('matakuliah/{matakuliah}', 'MatakuliahController@lihat');
+Route::post('matakuliah/simpan', 'MatakuliahController@simpan');
+Route::get('matakuliah/edit/{matakuliah}', 'MatakuliahController@edit');
+Route::post('matakuliah/edit/{matakuliah}', 'MatakuliahController@update');
+Route::get('matakuliah/hapus/{matakuliah}', 'MatakuliahController@hapus');
+
+//------------------------------------------------------------------
+
+//Route::get('ruangan/{ruangan}', 'RuanganController@lihat');
+Route::post('ruangan/simpan', 'RuanganController@simpan');
+Route::get('ruangan/edit/{ruangan}', 'RuanganController@edit');
+Route::post('ruangan/edit/{ruangan}', 'RuanganController@update');
+Route::get('ruangan/hapus/{ruangan}', 'RuanganController@hapus');
+
+//------------------------------------------------------------------
+
+Route::get('mahasiswa/{mahasiswa}', 'MahasiswaController@lihat');
+Route::post('mahasiswa/simpan', 'MahasiswaController@simpan');
+Route::get('mahasiswa/edit/{mahasiswa}', 'MahasiswaController@edit');
+Route::post('mahasiswa/edit/{mahasiswa}', 'MahasiswaController@update');
+Route::get('mahasiswa/hapus/{mahasiswa}', 'MahasiswaController@hapus');
+
+//------------------------------------------------------------------
+
+Route::get('dosen/{dosen}', 'DosenController@lihat');
+Route::post('dosen/simpan', 'DosenController@simpan');
+Route::get('dosen/edit/{dosen}', 'DosenController@edit');
+Route::post('dosen/edit/{dosen}', 'DosenController@update');
+Route::get('dosen/hapus/{dosen}', 'DosenController@hapus');
+
+//------------------------------------------------------------------
+
+Route::get('dosen_matakuliah/{dosen_matakuliah}', 'Dosen_matakuliahController@lihat');
+Route::post('dosen_matakuliah/simpan', 'Dosen_matakuliahController@simpan');
+Route::get('dosen_matakuliah/edit/{dosen_matakuliah}', 'Dosen_matakuliahController@edit');
+Route::post('dosen_matakuliah/edit/{dosen_matakuliah}', 'Dosen_matakuliahController@update');
+Route::get('dosen_matakuliah/hapus/{dosen_matakuliah}', 'Dosen_matakuliahController@hapus');
+
+//------------------------------------------------------------------
+
+Route::get('jadwal_matakuliah/{jadwal_matakuliah}', 'Jadwal_matakuliahController@lihat');
+Route::post('jadwal_matakuliah/simpan', 'Jadwal_matakuliahController@simpan');
+Route::get('jadwal_matakuliah/edit/{jadwal_matakuliah}', 'Jadwal_matakuliahController@edit');
+Route::post('jadwal_matakuliah/edit/{jadwal_matakuliah}', 'Jadwal_matakuliahController@update');
+Route::get('jadwal_matakuliah/hapus/{jadwal_matakuliah}', 'Jadwal_matakuliahController@hapus');
+
+/*
+Route::get('/', 'HomeController@index');
+
+Route::get('pengguna/{pengguna?}', function ($pengguna="Gino") {
+    return "Hello World dari Pengguna $pengguna";
+});
+
+Route::get('berita/{berita?}', function ($berita="laravel 5") {
+    return "Berita $berita belum dibaca";
+});
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::resource('anggota','AnggotaController');
+Route::resource('anggota.hobi','HobiController');
+*/
